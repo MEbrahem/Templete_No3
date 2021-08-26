d01586e417aa918d011f97871bebd8fd2a9b71a6
MEbrahem <me6283223@gmail.com>
2021-08-26 14:38:59 +0300

    First modify

--- js/index.js
+++ js/index.js
@@ -6,15 +6,26 @@ let ad = document.querySelector('.sitting-icon .icon'),
     box = document.querySelector('.sitting-icon');
 
 ad.addEventListener('click', function () {
-    box.classList.toggle('open');
-    ad1.classList.toggle('fa-spin');
+    box.classList.add('open');
+    $(ad).hide();
 })
 document.body.ondblclick = _ => {
     box.classList.remove('open');
-    ad1.classList.remove('fa-spin');
+    $(ad).show();
 }
+// jquery
+$('.sitting-icon .icon').after('<span> Sitting </span>');
+$('.sitting-icon .icon').hover(function () {
+    $('.sitting-icon .icon + span').fadeIn();
+},
+    function () {
+        $('.sitting-icon .icon + span').fadeOut();
+    }
+)
 
 
+
+///////////////////////////////////////////////////
 // change color
 
 
@@ -38,20 +49,83 @@ allcolor.forEach((v) => {
         e.target.classList.add('active');
     })
 })
+/////////////////////////////////////////////////
+// change random background
+var backchange = true, backInterval;
+let alloption = document.querySelectorAll('.sitting-back span');
+let option = localStorage.getItem('option');
 
+
+alloption.forEach(function (span) {
+    span.classList.remove('active');
+})
+if (option !== null) {
+    if (option === 'true') {
+        backchange = true;
+        alloption[0].classList.add('active');
+    }
+    else {
+        backchange = false;
+        alloption[1].classList.add('active');
+    }
+    console.log(option);
+}
+alloption.forEach((span) => {
+    span.addEventListener('click', (e) => {
+        e.target.parentElement.querySelectorAll('.active').forEach((ele) => {
+            ele.classList.remove('active');
+        })
+        e.target.classList.add('active');
+
+        if (e.target.dataset.back === 'yes') {
+            backchange = true;
+            changeBack();
+            console.log('yes');
+        }
+        else {
+            backchange = false;
+            clearInterval(backInterval);
+            console.log("no");
+        }
+        localStorage.setItem('option', backchange);
+    })
+
+})
 
 // End sitting-icon
 // start landing pages.
+// start fixed header
+
+let header = document.querySelector('.landing .header');
+let container = document.querySelector('.landing .container');
+let fix = document.querySelector('.landing .container .fix');
+
+window.onscroll = function () {
+    if (document.scrollingElement.scrollTop > 0) {
+        fix.style.backgroundColor = '#fff';
+        fix.style.zIndex = '5';
+
+    }
+    else {
+        fix.style.background = 'none';
+    }
+}
+
+
+// End fixed header
+//////////////////////////////////////////////
 // Start change background images.
-
-let img = ['no1.jpg', 'no2.jpg', 'no3.jpg', 'no4.jpg'];
-let land = document.getElementsByClassName('landing')[0];
-setInterval(_ => {
-    let i = Math.floor(Math.random() * img.length);
-    land.style.backgroundImage = `url(img/${img[i]})`;
-    // land.css('backgroundImage', `url(img/${img[i]})`);
-}, 3000);
-
+function changeBack() {
+    let img = ['no1.jpg', 'no2.jpg', 'no3.jpg', 'no4.jpg'];
+    let land = document.getElementsByClassName('landing')[0];
+    if (backchange === true) {
+        backInterval = setInterval(_ => {
+            let i = Math.floor(Math.random() * img.length);
+            land.style.backgroundImage = `url(img/${img[i]})`;
+        }, 3000);
+    }
+}
+changeBack();
 // End change background images.
 // start writer.
 
@@ -68,6 +142,74 @@ let writer = setInterval(_ => {
 
 // End writer.
 // End landing pages.
+
+
+//  start About us
+let Servics = document.querySelector('.service');
+let About = document.querySelector('.about_us');
+document.onscroll = function () {
+    if (document.scrollingElement.scrollTop >= 164) {
+        $('.about_us').animate({ "opacity": "1" }, 500);
+    }
+    // service
+    // window.pageYOffset > (Servics.offsetTop + Servics.offsetHeight - window.innerHeight
+    if (window.pageYOffset >= (Servics.offsetTop - Servics.offsetHeight + (0.5 * window.innerHeight))) {
+        let span = document.querySelectorAll('.service .skill .progress span');
+        span.forEach((s) => {
+            s.style.width = s.dataset.prog;
+        })
+
+    }
+    //service
+}
+
+
+//  End About us
+//  Start Servics
+
+// if (window.pageYOffset >= (Servics.offsetTop - Servics.offsetHeight + (0.5 * window.innerHeight))) {
+//     let span = document.querySelectorAll('.service .skill .progress span');
+//     span.forEach((s) => {
+//         s.style.width = s.dataset.prog;
+//     })
+
+// }
+
+//  End Servics
+//  Start products
+
+let img = document.querySelectorAll('.product .imge-box img');
+let over = document.createElement('div');
+let popup = document.createElement('div');
+let titel = document.createElement('h2');
+let photo = document.createElement('img');
+let close1 = document.createElement('span');
+
+img.forEach((I) => {
+    I.addEventListener('click', function () {
+        over.style.display = 'block';
+        over.classList.add('popup');
+        document.body.appendChild(over);
+        popup.classList.add("popup-box");
+        titel.classList.add('T-img');
+        titel.textContent = I.alt;
+        photo.classList.add('photo');
+        photo.src = I.src;
+        close1.classList.add("close");
+        close1.textContent = 'X';
+        popup.appendChild(titel);
+        popup.appendChild(close1);
+        popup.appendChild(photo);
+        over.appendChild(popup);
+    })
+    close1.addEventListener('click', function () {
+        over.style.display = "none";
+    })
+})
+
+//  End products
+
+
 
 
 
